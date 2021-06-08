78522f8d7295631683683858b7ec419d462c0ce3
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-08 14:19:09 +0530

    Add files via upload

---
+++ Alpha_Box2.java
@@ -0,0 +1,15 @@
+class Alpha_Box2
+{
+public static void main(String[] args)
+{
+ for(int i=1; i<=5; i++)
+{
+  for(int j=1; j<=5; j++)
+{
+System.out.print((char)(j+64)+" ");
+
+}
+System.out.println();
+}
+}
+}
